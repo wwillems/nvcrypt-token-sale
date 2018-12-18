2ef360a9ec496e0660c7a821cebc1271a29ec0cc
wwillems <william.willems@gmail.com>
2018-12-18 09:44:14 +0100

    finished frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,133 @@
-console.log("app.js loaded");
+App= {
+	web3Provider: null,
+	contracts: {},
+	account: '0x0',
+	loading: false,
+	tokenPrice: 1000000000000000,
+	tokensSold: 0,
+	tokensAvailable: 750000,
+
+	init: function() {
+		console.log("App initialized...");
+		return App.initWeb3();
+	},
+
+	initWeb3: function() {
+	    if (typeof web3 !== 'undefined') {
+	      // If a web3 instance is already provided by Meta Mask.
+	      App.web3Provider = web3.currentProvider;
+	      web3 = new Web3(web3.currentProvider);
+	    } else {
+	      // Specify default instance if no web3 instance provided
+	      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+	      web3 = new Web3(App.web3Provider);
+	    }
+	    return App.initContracts();
+	},
+
+	initContracts: function() {
+		$.getJSON("NVCryptTokenSale.json", function(nvcryptTokenSale) {
+			App.contracts.NVCryptTokenSale = TruffleContract(nvcryptTokenSale);
+			App.contracts.NVCryptTokenSale.setProvider(App.web3Provider);
+			App.contracts.NVCryptTokenSale.deployed().then(function(nvcryptTokenSale) {
+			  console.log("NVCrypt Token Sale Address:", nvcryptTokenSale.address);
+			})
+		}).done(function() {
+			$.getJSON("NVCryptToken.json", function(nvcryptToken) {
+				App.contracts.NVCryptToken = TruffleContract(nvcryptToken);
+				App.contracts.NVCryptToken.setProvider(App.web3Provider);
+				App.contracts.NVCryptToken.deployed().then(function(nvcryptToken) {
+				  console.log("NVCrypt Token Address:", nvcryptToken.address);
+				});
+
+				App.listenForEvents();
+				return App.render();
+			})
+		})
+	},
+
+	// Listen for events emitted from the contract
+	listenForEvents: function() {
+		App.contracts.NVCryptTokenSale.deployed().then(function(instance) {
+		  instance.Sell({}, {
+		    fromBlock: 0,
+		    toBlock: 'latest',
+		  }).watch(function(error, event) {
+		    console.log("event triggered", event);
+		    App.render();
+		  })
+		})
+	},
+
+	render: function() {
+		if (App.loading) {
+			return;
+		}
+		App.loading = true;
+
+		var loader = $('#loader');
+		var content = $('#content');
+
+		loader.show();
+		content.hide();
+
+		// Load account data
+		web3.eth.getCoinbase(function(err, account) {
+			if (err===null) {
+				App.account = account;
+				$('#accountAddress').html("Your Account: " + account);
+			}
+		})
+
+	    // Load token sale contract
+	    App.contracts.NVCryptTokenSale.deployed().then(function(instance) {
+	      nvcryptTokenSaleInstance = instance;
+	      return nvcryptTokenSaleInstance.tokenPrice();
+	    }).then(function(tokenPrice) {
+	      App.tokenPrice = tokenPrice;
+	      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+	      return nvcryptTokenSaleInstance.tokensSold();
+	    }).then(function(tokensSold) {
+	      App.tokensSold = tokensSold.toNumber();
+	      $('.tokens-sold').html(App.tokensSold);
+	      $('.tokens-available').html(App.tokensAvailable);
+
+	      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+	      $('#progress').css('width', progressPercent + '%');
+
+	      // Load token contract
+	      App.contracts.NVCryptToken.deployed().then(function(instance) {
+	        nvcryptTokenInstance = instance;
+	        return nvcryptTokenInstance.balanceOf(App.account);
+	      }).then(function(balance) {
+	        $('.nvc-balance').html(balance.toNumber());
+	        App.loading = false;
+	        loader.hide();
+	        content.show();
+	      })
+	     }) 
+	},
+
+	buyTokens: function() {
+		$('#content').hide();
+		$('#loader').show();
+		var numberOfTokens = $('#numberOfTokens').val();
+		App.contracts.NVCryptTokenSale.deployed().then(function(instance) {
+			return instance.buyTokens(numberOfTokens, {
+				from: App.account,
+				value: numberOfTokens * App.tokenPrice,
+				gas: 500000 // Gas limit
+			});
+		}).then(function(result) {
+			console.log("Tokens bought...")
+			$('form').trigger('reset') // reset number of tokens in form
+			// Wait for Sell event
+		});
+	}
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  })
+});
